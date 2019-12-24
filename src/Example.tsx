767da903aebdd7e1503a85e44dc4d30bd724aed8
Daniel Boyd <daniel.boyd@centralreach.com>
2019-12-23 23:28:12 -0600

    init

---
+++ src/Example.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+import React from "react";
+
+export const Example: FC = () =>
+    <h1>Example</h1>
---
+++ src/Home.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+import React from "react";
+
+export const Home: FC = () => 
+        <h1>Hallo</h1>
